ac8cc03c6fa97e68c20a9397eb3895f091624e67
Bianka <126699601+binka66@users.noreply.github.com>
2023-10-09 17:21:17 -0300

    Trabalho 1 questão 3

---
+++ Trabalho 1/Q3/fila.h
@@ -0,0 +1,21 @@
+
+typedef struct lista Lista;
+typedef struct fila Fila;
+
+Fila* fila_cria(void);
+/*Testa se uma Fila é vazia.*/
+int fila_vazia(Fila *f);
+/*Função que adiciona um elemento em uma Fila.*/
+void fila_insere(Fila *f, int info);
+/*Função que remove um elemento de uma Fila.*/
+int fila_remove(Fila *f);
+/*Função que imprime os elementos de uma Fila.*/
+void fila_imprime(Fila *f);
+/*Libera o espaço alocado para uma Fila.*/
+void fila_libera(Fila *f);
+
+int qtd_maior(Fila* f, int n);
+
+Fila* inverte(Fila* f);
+
+int pares(Fila* f);
